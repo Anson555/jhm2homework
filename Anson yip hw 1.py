316cebd580ec334fe6d6f45798bf1186d7fc1571
Anson555 <myipinghei112730@gmail.com>
2025-02-08 10:09:47 +0800

    Create Anson yip hw 1.py

---
+++ Anson yip hw 1.py
@@ -0,0 +1,8 @@
+priceA=input("")
+priceB=input("")
+if priceA<priceB
+  print("priceA is larger than priceB)
+if priceA>priceB
+    print("priceA is smaller than priceB)
+else if  
+      print("The prices are same")
